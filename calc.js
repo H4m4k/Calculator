31789a58a004c2c65042c6a16c0be9f15e322a42
H4m4k <73617515+H4m4k@users.noreply.github.com>
2020-11-09 20:27:42 +0100

    Multiple dots prevention is done

--- calc.js
+++ calc.js
@@ -5,22 +5,37 @@ let display = document.querySelector('#display');
 
 let keyboard = document.querySelector('#keyboard');
 
-//memory for calculations
 
-let memory = 0;
+
 
 keyboard.addEventListener('click' , e => {
 
   if (e.target.classList.contains('number') ) {
 
-    if (display.textContent === '0') display.textContent = '';
-    display.textContent += e.target.textContent
+    switch (display.textContent) {
+      case '0':
+        display.textContent = '';
+        break;
+    }
+
+    /* 
+     Prevent the possibility of adding multiple decimal separators (dots)
+    */
+    if ( display.textContent.includes('.') && e.target.textContent === '.') {
+ 
+      display.textContent = display.textContent + '';
+      
+    } else {
+
+      display.textContent += e.target.textContent;
+
+    }
 
   } else if ( e.target.classList.contains('sign')) {  // change of sign by mutliplying with -1
 
     display.textContent *= -1;
 
-  } 
+  }
 });
 
 
@@ -30,20 +45,32 @@ let functions = document.querySelector('#functions');
 
 functions.addEventListener('click', e => {
 
-  if ( e.target.textContent === 'C' ) display.textContent = '0'; // clear the display
+  switch (e.target.textContent) {
 
-  if ( e.target.textContent === 'x2' ) {
-    let num = display.textContent ** 2;
-    return screen(num);
-  }// exponent w/screen Function
+    case 'C':
+      return screen(0);
+
+    case 'x2':
+      let num = display.textContent ** 2;
+      return screen(num);
+  }
+
   
   if ( e.target.classList.contains('sqrt')) {
-    let num = Math.sqrt(display.textContent);
-    return screen(num);
+    squareRoot(display.textContent);
+
   } // square root w/screen Function
   
   });
 
+  function squareRoot(number) {
+    let num = Math.sqrt(number);
+    return screen(num);
+  }
+
+  function clear(e) {
+    display.textContent = e.target.textContent;
+  }
 
 
   
@@ -60,7 +87,6 @@ functions.addEventListener('click', e => {
     } else {
       return display.textContent = result;
     }
-    
   } // end of screen
 
 
@@ -87,6 +113,7 @@ extFunctions.addEventListener('click',e => {
    3 - need to set precision of (any)/exponential numbers                   // done
     3a - create a display function to solve the precision in one place      // done
     3b - upgrade the precision of numbers to keep in the screen size 
-    3c - prevent multiple dots 
+    3c - prevent multiple dots                                              // done
+    3d - prevent adding numbers after a operation has been performed
    4 - solve the functions different size of buttons
    */
